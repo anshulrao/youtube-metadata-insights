755ef389e5dc13a9a56368162d2bcbf4c9968e0a
anshulrao <anshul_rao@ymail.com>
2023-01-21 14:31:55 -0500

    add new files

--- merge_files.py
+++ merge_files.py
@@ -2,8 +2,6 @@ import glob
 import pandas as pd
 import datetime
 
-# df = pd.read_csv("YouTubeDataset_withChannelElapsed.csv")
-
 tbls = []
 for f in glob.glob("data/channel_*.csv.gz"):
     try:
@@ -12,7 +10,6 @@ for f in glob.glob("data/channel_*.csv.gz"):
         pass
 c = pd.concat(tbls)
 c = c.rename(columns={"channel_id": "channelId"})
-# df = df.merge(c, how="outer", on="channelId")
 
 tbls = []
 for f in glob.glob("data/video_*.csv.gz"):
@@ -22,9 +19,7 @@ for f in glob.glob("data/video_*.csv.gz"):
         pass
 v = pd.concat(tbls)
 v = v.rename(columns={"video_id": "videoId"})
-# df = df.merge(v, how="outer", on="videoId")
 
-# df.to_csv("YouTubeDataset_withChannelElapsedAppended.csv.gz")
 c.to_csv(f"channel_data_{datetime.datetime.now().strftime('%Y_%m_%d-%I_%M_%S_%p')}.csv.gz", compression="gzip",
          index=False)
 v.to_csv(f"video_data_{datetime.datetime.now().strftime('%Y_%m_%d-%I_%M_%S_%p')}.csv.gz", compression="gzip",
